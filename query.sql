284cb271f4f36546d4847a65da4b409913067d65
Max12121 <43761868+Max12121@users.noreply.github.com>
2022-03-22 21:47:42 +0100

    username unique

--- query.sql
+++ query.sql
@@ -3,7 +3,7 @@ USE mydb;
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) NOT NULL,
+  `username` varchar(32) NOT NULL UNIQUE,
   `password` varchar(32) NOT NULL,
    PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
