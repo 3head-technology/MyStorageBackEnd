4f8e23346ac54247daec1f36fe7110f4aa0d6297
Max12121 <43761868+Max12121@users.noreply.github.com>
2022-03-22 20:38:37 +0100

    Create user table

---
+++ query.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+   PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
